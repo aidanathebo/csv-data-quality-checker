f4df432517846c343f7cecb7f718ffeeea508bd2
Aidana <iaidana.new@gmail.com>
2026-09-13 00:24:55 +0300

    function + missing values

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,26 @@
 import pandas as pd
 
 
-df = pd.read_csv("data/example.csv")
+def analyze_csv(file_path):
+    df = pd.read_csv(file_path)
 
-print("Rows:", len(df))
-print("Columns:", len(df.columns))
+    print("Rows:", len(df))
+    print("Columns:", len(df.columns))
 
-print("\nColumn names:")
-for column in df.columns:
-    print("-", column)
+    print("\nColumn names:")
+    for column in df.columns:
+        print("-", column)
+
+    missing = df.isnull().sum()
+    missing_percentage = (missing / len(df)) * 100
+
+    print("\nMissing values:")
+
+    for column in df.columns:
+        count = missing[column]
+        percentage = missing_percentage[column]
+
+        print(f"  {column}: {count} ({percentage:.1f}%)")
+
+
+analyze_csv("data/example.csv")
